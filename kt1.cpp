36e18c5491d349fb2a9b40251bd74bad9e766590
sonkajarvi <sonkajarvi@hotmail.com>
2024-01-12 15:48:38 +0200

    add kt1.cpp
    also add .gitignore

---
+++ kt1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ctime>
+
+
+int game(int max)
+{
+    std::srand(std::time(nullptr));
+    int number = std::rand() % (max + 1);
+
+    int guess = 0;
+    int tries = 0;
+
+    do {
+        std::cout << "arvaa luku (0-" << max << "):";
+        std::cin >> guess;
+
+        if (guess < number) std::cout << "luku on suurempi" << std::endl;
+        else if (guess > number) std::cout << "luku on pienempi" << std::endl;
+        tries++;
+    } while (guess != number);
+
+    std::cout << "oikea vastaus!" << std::endl;
+
+    return tries;
+}
+
+
+int main()
+{
+    int tries = game(40);
+    std::cout << "yritykset: " << tries << std::endl;
+}
